7d517f5acb692ec60152e72947dac2ff2bf2be57
YoussefAmin2312 <youssefyasser98989@gmail.com>
2024-10-22 20:10:32 +0300

    pages  creation

---
+++ lib/request.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class RequestScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Requests'),
+      ),
+      body: Center(
+        child: Text('Requests'),
+      ),
+    );
+  }
+}
